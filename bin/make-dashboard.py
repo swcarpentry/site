fffd234996000d1ec9824a587333b5aec75e18b5
Greg Wilson <gvwilson@third-bit.com>
2015-01-04 05:30:47 -0800

    Allowing interactive use

--- bin/make-dashboard.py
+++ bin/make-dashboard.py
@@ -5,9 +5,9 @@
 import sys
 import yaml
 from github import Github
-from util import DASHBOARD_CACHE
+from optparse import OptionParser
 
-controls = (
+CONTROLS = (
     ('swcarpentry/shell-novice', 'Introduction to the Unix shell'),
     ('swcarpentry/git-novice', 'Introduction to Git'),
     ('swcarpentry/hg-novice', 'Introduction to Mercurial'),
@@ -23,21 +23,46 @@ controls = (
     ('swcarpentry/site', 'Software Carpentry website'),
 )
 
-assert len(sys.argv) == 3, 'Usage: make-dashboard.py token-file output-file'
-token_file = sys.argv[1]
-output_file = sys.argv[2]
+USAGE = '''Usage: make-dashboard.py -o outputfile [-t tokenfile | -u username -p password]
+If no credentials are supplied, user is prompted for username and password.
+To store credentials, create a file called git-token.txt that contains your GitHub API token
+and put it in the root directory of the 'site' project.'''
 
-with open(token_file, 'r') as reader:
-    token = reader.read().strip()
+# Command-line options.
+parser = OptionParser()
+parser.add_option('-o', '--output', dest='output', help='output directory')
+parser.add_option('-t', '--tokenfile', dest='tokenfile', help='GitHub token file')
+parser.add_option('-u', '--username', dest='username', help='GitHub username')
+parser.add_option('-p', '--password', dest='password', help='GitHub password')
+options, args = parser.parse_args()
 
-g = Github(token)
+assert not args, USAGE
+
+if options.tokenfile is not None:
+    assert options.username is None and options.password is None, USAGE
+    with open(token_file, 'r') as reader:
+        token = reader.read().strip()
+        g = Github(token)
+
+elif options.username is not None and options.password is not None:
+    g = Github(options.username, options.password)
+
+elif options.username is None and options.password is None:
+    username = raw_input('username: ')
+    password = raw_input('password: ')
+    g = Github(username, password)
+
+else:
+    assert False, USAGE
+    
+# Process data.
 all_records = []
 dashboard = {
     'records' : all_records,
     'num_repos' : 0,
     'num_issues' : 0
 }
-for (ident, description) in controls:
+for (ident, description) in CONTROLS:
     print '+', ident
     dashboard['num_repos'] += 1
     r = g.get_repo(ident)
@@ -54,5 +79,6 @@ for (ident, description) in controls:
         dashboard['num_issues'] += 1
     record['issues'].sort(lambda x, y: - cmp(x['updated'], y['updated']))
 
+# Output.
 with open(output_file, 'w') as writer:
     yaml.dump(dashboard, writer, encoding='utf-8', allow_unicode=True)
